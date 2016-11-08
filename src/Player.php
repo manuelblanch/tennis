269a96e171b1a80ce4550e3c72fa035f3081e777
mn <manuelblanch@iesebre.com>
2016-11-08 16:25:50 +0000

    Applied fixes from StyleCI

--- src/Player.php
+++ src/Player.php
@@ -1,17 +1,20 @@
 <?php
+
 class Player
 {
     protected $code = [
-        0 =>"0",
-        1 =>"15",
-        2 => "30",
-        3 => "40"
+        0 => '0',
+        1 => '15',
+        2 => '30',
+        3 => '40',
     ];
     protected $points;
     protected $score;
     protected $name;
+
     /**
      * Player constructor.
+     *
      * @param $score
      * @param $name
      */
@@ -20,16 +23,24 @@ class Player
         $this->points = $points;
         $this->name = $name;
     }
-    public function winpoint(){
+
+    public function winpoint()
+    {
         $this->points += 1;
     }
-    public function getScore(){
+
+    public function getScore()
+    {
         return true;
     }
-    public function points(){
+
+    public function points()
+    {
         return $this->code[$this->points];
     }
-    public function name(){
+
+    public function name()
+    {
         return $this->code[$this->name];
     }
 }
